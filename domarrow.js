8b1af059a317be3b231cdf7921a6416a35d0c10a
Béla Schaum <schaumb@gmail.com>
2019-01-30 05:11:58 +0100

    connected tag observer, text msg, size..

--- domarrow.js
+++ domarrow.js
@@ -39,10 +39,21 @@
     var color = trafo && trafo.color || 'black';
     var W = trafo && trafo.width || 2;
 
-    var fT = from.offsetTop + from.offsetHeight * getNumberOrDef(trafo && trafo.fromY, getNumberOrDef(trafo, 0.5));
-    var tT = to.offsetTop + to.offsetHeight * getNumberOrDef(trafo && trafo.toY, getNumberOrDef(trafo, 0.5));
-    var fL = from.offsetLeft + from.offsetWidth * getNumberOrDef(trafo && trafo.fromX, getNumberOrDef(trafo, 0.5));
-    var tL = to.offsetLeft + to.offsetWidth * getNumberOrDef(trafo && trafo.toX, getNumberOrDef(trafo, 0.5));
+    var fromB = parseFloat(from.style.top) ? null : from.getBoundingClientRect();
+    var toB = parseFloat(to.style.top) ? null : to.getBoundingClientRect();
+    var fromBStartY = (fromB ? window.scrollY + fromB.top : parseFloat(from.style.top));
+    var fromBStartX = (fromB ? window.scrollX + fromB.left : parseFloat(from.style.left));
+    var toBStartY = (toB ? window.scrollY + toB.top : parseFloat(to.style.top));
+    var toBStartX = (toB ? window.scrollX + toB.left : parseFloat(to.style.left));
+    var fromBWidth = (fromB ? fromB.width : parseFloat(from.style.width) || from.offsetWidth);
+    var fromBHeight = (fromB ? fromB.height : parseFloat(from.style.height) || from.offsetHeight);
+    var toBWidth = (toB ? toB.width : parseFloat(to.style.width) || to.offsetWidth);
+    var toBHeight = (toB ? toB.height : parseFloat(to.style.height) || to.offsetHeight);
+
+    var fT = fromBStartY + fromBHeight * getNumberOrDef(trafo && trafo.fromY, getNumberOrDef(trafo, 0.5));
+    var tT = toBStartY + toBHeight * getNumberOrDef(trafo && trafo.toY, getNumberOrDef(trafo, 0.5));
+    var fL = fromBStartX + fromBWidth * getNumberOrDef(trafo && trafo.fromX, getNumberOrDef(trafo, 0.5));
+    var tL = toBStartX + toBWidth * getNumberOrDef(trafo && trafo.toX, getNumberOrDef(trafo, 0.5));
 
     var CA   = Math.abs(tT - fT);
     var CO   = Math.abs(tL - fL);
@@ -54,8 +65,8 @@
     }
 
     if (trafo && trafo.onlyVisible) {
-      var arrangeFrom = intersectionPoint(fL, fT, tL, tT, from.offsetLeft, from.offsetTop, from.offsetLeft + from.offsetWidth, from.offsetTop + from.offsetHeight);
-      var arrangeTo = intersectionPoint(fL, fT, tL, tT, to.offsetLeft, to.offsetTop, to.offsetLeft + to.offsetWidth, to.offsetTop + to.offsetHeight);
+      var arrangeFrom = intersectionPoint(fL, fT, tL, tT, fromBStartX, fromBStartY, fromBStartX + fromBWidth, fromBStartY + fromBHeight);
+      var arrangeTo = intersectionPoint(fL, fT, tL, tT, toBStartX, toBStartY, toBStartX + toBWidth, toBStartY + toBHeight);
 
       if (arrangeFrom) {
         fL = arrangeFrom.x;
@@ -82,13 +93,15 @@
       arrowFw.classList.remove('arrow-bw');
       arrowFw.classList.add('arrow-fw');
       arrowFw.style.borderRightColor = color;
+      arrowFw.style.top = W/2-6 + "px";
     }
     if (arrowBw) {
       arrowBw.classList.remove('arrow-fw');
       arrowBw.classList.add('arrow-bw');
       arrowBw.style.borderLeftColor = color;
+      arrowBw.style.top = W/2-6 + "px";
     }
-
+    line.style.display = "none";
     line.style["-webkit-transform"] = 'rotate('+ ANG +'deg)';
     line.style["-moz-transform"] = 'rotate('+ ANG +'deg)';
     line.style["-ms-transform"] = 'rotate('+ ANG +'deg)';
@@ -99,9 +112,10 @@
     line.style.width  = H + 'px';
     line.style.height = W + 'px';
     line.style.background = "linear-gradient(to right, " +
-      (arrowFw ? "transparent" : color) +" 10px, " +
-      color + " 10px " + (H - 10) + "px, " +
-      (arrowBw ? "transparent" : color) + " " + (H - 10) + "px 100%)";
+      (arrowFw ? "transparent" : color) +" 11px, " +
+      color + " 11px " + (H - 11) + "px, " +
+      (arrowBw ? "transparent" : color) + " " + (H - 11) + "px 100%)";
+    line.style.display = "initial";
   }
 
   function repaintConnection(connectionElement) {
@@ -109,6 +123,8 @@
     var fromE = document.querySelector(fromQ);
     var toQ = connectionElement.getAttribute('to');
     var toE = document.querySelector(toQ);
+    connectedObserver.observe(fromE, {attributes:true});
+    connectedObserver.observe(toE, {attributes:true});
 
     var lineE = connectionElement.getElementsByClassName('line')[0];
     if (!lineE) {
@@ -136,6 +152,18 @@
     tail && (tail.style.visibility = needTail ? 'visible' : 'hidden');
     head && (head.style.visibility = needHead ? 'visible' : 'hidden');
 
+    var textE = lineE.getElementsByClassName('text')[0];
+    var textMessage = connectionElement.getAttribute('text');
+
+    if (!textE && textMessage) {
+      textE = document.createElement('div');
+      textE.classList.add('text');
+      lineE.appendChild(textE);
+    }
+    if (textE && textE.innerText != textMessage) {
+      textE.innerText = textMessage;
+    }
+
     adjustLine(fromE, toE, lineE, {
       color: connectionElement.getAttribute('color'),
       onlyVisible: connectionElement.hasAttribute('onlyVisible'),
@@ -145,6 +173,12 @@
       toY: parseFloat(connectionElement.getAttribute('toY')),
       width: parseFloat(connectionElement.getAttribute('width'))
     });
+  }
+
+  function repaintWithoutObserve(tag) {
+    connectionObserver.observe(tag, {attributeFilter: []});
+    repaintConnection(tag);
+    connectionObserver.observe(tag, {attributes:true, childList:true, subtree: true});
   }
 
   function createOne(newElement) {
@@ -166,6 +200,29 @@
     connectionObserver.observe(tag, {attributeFilter: []});
   }
 
+  function changedConnectedTag(changes) {
+    changes.forEach(e => {
+      var changedElem = e.target;
+      var wasConnection = false;
+      for (var i = 0; i < connectionElements.length; ++i) {
+        var fromE = document.querySelector(connectionElements[i].getAttribute('from'));
+        if (fromE === changedElem) {
+          wasConnection = true;
+          repaintWithoutObserve(connectionElements[i]);
+          continue;
+        }
+        var toE = document.querySelector(connectionElements[i].getAttribute('to'));
+        if (toE === changedElem) {
+          wasConnection = true;
+          repaintWithoutObserve(connectionElements[i]);
+        }
+      }
+      if (!wasConnection) {
+        connectionObserver.observe(changedElem, {attributeFilter: []});
+      }
+    });
+  }
+
   function changedConnectionTag(changes) {
     changes.forEach(e => {
       var conn = e.target;
@@ -174,20 +231,18 @@
       while (conn && conn.tagName.toLowerCase() !== 'connection')
         conn = conn.parentElement;
       if (!conn) return;
-      connectionObserver.observe(conn, {attributeFilter: []});
-      repaintConnection(conn);
-      connectionObserver.observe(conn, {attributes:true, childList:true, subtree: true});
+      repaintWithoutObserve(conn);
     });
   }
 
   function bodyNewElement(changes) {
     changes.forEach(e => {
       e.removedNodes.forEach(n => {
-        if (n.tagName.toLowerCase() === 'connection')
+        if (n.tagName && n.tagName.toLowerCase() === 'connection')
           removeConnection(n);
       });
       e.addedNodes.forEach(n => {
-        if (n.tagName.toLowerCase() === 'connection')
+        if (n.tagName && n.tagName.toLowerCase() === 'connection')
           createOne(n);
       });
     });
@@ -198,6 +253,7 @@
     window.WebKitMutationObserver || window.MozMutationObserver;
   var bodyObserver = new MutationObserver(bodyNewElement);
   var connectionObserver = new MutationObserver(changedConnectionTag);
+  var connectedObserver = new MutationObserver(changedConnectedTag);
   document.body && create() || window.addEventListener("load", create);
 })();
 
